46ca6ee99b683921715064563b1fef368e28c0b5
xig1517 <xig1517@gmail.com>
2025-04-26 21:05:05 +0800

    test: add Gametest - `gametest.mcstructure` - an empty structure for test - add dependency of `@minecraft/server-gametest 1.0.0-beta` - add npm script `typecheck` to do typecheck - `Test.ts` for testcases

---
+++ src/test/Test.ts
@@ -0,0 +1,15 @@
+import * as GameTest from "@minecraft/server-gametest";
+
+class DatabaseTest {
+
+    static readonly TEST_CASES = [];
+
+}
+
+function setup(builders: GameTest.RegistrationBuilder[]) {
+    for (const builder of builders) {
+        builder.structureName('xigmadb:gametest').tag('test');
+    }
+}
+
+setup(DatabaseTest.TEST_CASES);
