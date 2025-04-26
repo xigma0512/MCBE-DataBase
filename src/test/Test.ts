a6f398b0057a5c0e3705b00fc92d4ecbe7090de8
xig1517 <xig1517@gmail.com>
2025-04-27 00:02:54 +0800

    test: 重構 `Test.ts` 以符合新的程式架構

--- src/test/Test.ts
+++ src/test/Test.ts
@@ -1,5 +1,5 @@
 import * as GameTest from "@minecraft/server-gametest";
-import { Database } from "../Database";
+import { DatabaseManager } from "../Database";
 
 class DatabaseTest {
 
@@ -7,17 +7,19 @@ class DatabaseTest {
         GameTest.register(this.name, 'setting_data', (test: GameTest.Test) => {
             // basic setting function
             test.succeedIf(() => {
-                const db = new Database<string>('foobar');
+                const db = DatabaseManager.instance.getDatabase<string>('foobar');
                 db.set('foo', 'bar');
                 test.assert(db.get('foo') === 'bar', "db.get('foo') should equal to 'bar'.");
             });
         }),
         GameTest.register(this.name, 'data_consistency', (test: GameTest.Test) => {
-            // two instance using same database, the change should be update
-            const inst = new Database<string>('foobar2');
-            const other_inst = new Database<string>('foobar2');
-            inst.set('foo', 'bar2');
-            test.assert(other_inst.get('foo') === inst.get('foo'), 'the value of key "foo" should be the same');
+            test.succeedIf(() => {
+                // two instance using same database, the change should be update
+                const inst = DatabaseManager.instance.getDatabase<string>('foobar2');
+                const other_inst = DatabaseManager.instance.getDatabase<string>('foobar2');
+                inst.set('foo', 'bar2');
+                test.assert(other_inst.get('foo') === inst.get('foo'), 'the value of key "foo" should be the same');
+            });
         })
     ];
 
@@ -25,7 +27,7 @@ class DatabaseTest {
 
 function setup(builders: GameTest.RegistrationBuilder[]) {
     for (const builder of builders) {
-        builder.structureName('db:gametest').tag('test');
+        builder.structureName('db:gametest').tag('database_test');
     }
 }
 
